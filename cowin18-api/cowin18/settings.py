7ff7af6721837cbe0d1c1a0f73f74f08252eb1a3
Alex Mathew <alexmathew003@gmail.com>
2021-04-28 20:54:24 +0530

    Query centers endpoint and add to state

--- cowin18-api/cowin18/settings.py
+++ cowin18-api/cowin18/settings.py
@@ -29,6 +29,8 @@ DEBUG = True if os.environ.get("DEBUG") == "true" else False
 
 ALLOWED_HOSTS = []
 
+CORS_ORIGIN_ALLOW_ALL = True
+
 
 # Application definition
 
@@ -39,12 +41,14 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "corsheaders",
     "centers",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
--- cowin18-dashboard/src/App.js
+++ cowin18-dashboard/src/App.js
@@ -1,6 +1,17 @@
 import React from "react";
+import cowin18 from "./api/cowin18";
 
 class App extends React.Component {
+  state = {
+    centers: {},
+  };
+
+  async componentDidMount() {
+    const resp = await cowin18.get("/centers/");
+    const centers = resp.data;
+    this.setState({ centers });
+  }
+
   render() {
     return <div>App.</div>;
   }

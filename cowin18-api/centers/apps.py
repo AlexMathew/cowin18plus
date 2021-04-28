c7b726d494179998a3efbe5b5715db247fcd71b9
Alex Mathew <alexmathew003@gmail.com>
2021-04-28 19:32:46 +0530

    Set up docker + django

---
+++ cowin18-api/centers/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CentersConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'centers'
---
+++ cowin18-api/cowin18/celery.py
@@ -0,0 +1,19 @@
+from __future__ import absolute_import
+import os
+from celery import Celery
+from celery.schedules import crontab
+from django.conf import settings
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cowin18.settings")
+
+REDIS_URL = f"redis://{os.getenv('REDIS_HOST')}:{os.getenv('REDIS_PORT')}/0"
+
+app = Celery("cowin18")
+
+app.conf.broker_url = REDIS_URL
+app.conf.result_expires = 3600
+app.conf.timezone = settings.TIME_ZONE
+# app.conf.imports = ""
+app.conf.beat_schedule = {}
+
+app.autodiscover_tasks()

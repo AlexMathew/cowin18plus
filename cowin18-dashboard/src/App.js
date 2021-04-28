b55dc3e421352336a72177626484d38a54c8c79a
Alex Mathew <alexmathew003@gmail.com>
2021-04-29 01:46:31 +0530

    Add district selector dropdown

--- cowin18-dashboard/src/App.js
+++ cowin18-dashboard/src/App.js
@@ -1,26 +1,79 @@
 import React from "react";
+import _ from "lodash";
+import { withStyles } from "@material-ui/core/styles";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormHelperText from "@material-ui/core/FormHelperText";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
 import cowin18 from "./api/cowin18";
 import CentersTable from "./CentersTable";
 
+const styles = (theme) => ({
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+  },
+});
+
 class App extends React.Component {
   state = {
-    centers: [],
+    districts: [],
+    districtsById: {},
+    centers: {},
     selectedDistrict: "",
+    selectedDistrictName: "",
   };
 
   async componentDidMount() {
     const resp = await cowin18.get("/centers/");
-    const centers = resp.data;
-    this.setState({ centers, selectedDistrict: "DISTRICT_571" });
+    const centers = resp.data.centers;
+    const districts = resp.data.districts;
+    this.setState({
+      districts,
+      districtsById: _.keyBy(districts, "id"),
+      centers,
+    });
   }
 
+  handleDistrictSelection = (event) => {
+    const districtId = event.target.value;
+    this.setState({
+      selectedDistrict: districtId,
+      selectedDistrictName: this.state.districtsById?.[districtId]?.name,
+    });
+  };
+
   render() {
+    const { classes } = this.props;
+
     return (
-      <CentersTable
-        data={this.state.centers?.[this.state.selectedDistrict] ?? []}
-      />
+      <div>
+        <FormControl className={classes.formControl}>
+          <InputLabel id="city-selector-label">City</InputLabel>
+          <Select
+            labelId="city-selector-label"
+            id="city-selector"
+            value={this.state.selectedDistrict}
+            onChange={this.handleDistrictSelection}
+          >
+            {this.state.districts.map((district) => (
+              <MenuItem key={district.id} value={district.id}>
+                {district.name}
+              </MenuItem>
+            ))}
+          </Select>
+          <FormHelperText>Select City</FormHelperText>
+        </FormControl>
+        <CentersTable
+          data={
+            this.state.centers?.[`DISTRICT_${this.state.selectedDistrict}`] ??
+            []
+          }
+        />
+      </div>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);

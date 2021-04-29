8aff84b32b6143a7df7eb122b20db15abfd9444d
Alex Mathew <alexmathew003@gmail.com>
2021-04-29 14:28:45 +0530

    Sort districts list

--- cowin18-api/centers/constants.py
+++ cowin18-api/centers/constants.py
@@ -16,24 +16,28 @@ directory = os.path.dirname(__file__)
 #                     district["district_id"] for district in districts["districts"]
 #                 ]
 #                 DISTRICT_IDS.extend(district_ids)
-DISTRICT_IDS = [
-    571,  # Chennai
-    265,  # Bangalore Urban
-    395,  # Mumbai
-    307,  # Ernakulam
-    140,  # New Delhi
-    581,  # Hyderabad
-    725,  # Kolkata
-]
-DISTRICT_IDS_AND_DISTRICTS = [
-    {"id": 571, "name": "Chennai"},
-    {"id": 265, "name": "Bangalore Urban"},
-    {"id": 395, "name": "Mumbai"},
-    {"id": 307, "name": "Ernakulam"},
-    {"id": 140, "name": "New Delhi"},
-    {"id": 581, "name": "Hyderabad"},
-    {"id": 725, "name": "Kolkata"},
-]
+# DISTRICT_IDS = [
+#     571,  # Chennai
+#     265,  # Bangalore Urban
+#     395,  # Mumbai
+#     307,  # Ernakulam
+#     140,  # New Delhi
+#     581,  # Hyderabad
+#     725,  # Kolkata
+# ]
+DISTRICT_IDS_AND_DISTRICTS = sorted(
+    [
+        {"id": 571, "name": "Chennai"},
+        {"id": 265, "name": "Bangalore Urban"},
+        {"id": 395, "name": "Mumbai"},
+        {"id": 307, "name": "Ernakulam"},
+        {"id": 140, "name": "New Delhi"},
+        {"id": 581, "name": "Hyderabad"},
+        {"id": 725, "name": "Kolkata"},
+    ],
+    key=lambda district: district["name"],
+)
+DISTRICT_IDS = [district["id"] for district in DISTRICT_IDS_AND_DISTRICTS]
 
 DISTRICT_KEY = lambda district_id: f"DISTRICT_{district_id}"
 

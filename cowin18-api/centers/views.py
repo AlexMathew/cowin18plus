5039531f91a8ef9e2cabfd2f5aeffe72ea281740
Alex Mathew <alexmathew003@gmail.com>
2021-04-30 14:30:30 +0530

    Update centers list endpoint to return by state id

--- cowin18-api/centers/views.py
+++ cowin18-api/centers/views.py
@@ -3,13 +3,12 @@ import json
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 
 from helpers.instances import redis
 
 from .constants import (
-    DISTRICT_IDS,
     DISTRICT_KEY,
-    DISTRICT_IDS_AND_DISTRICTS,
     DISTRICT_UPDATE_TIME_KEY,
 )
 
@@ -37,25 +36,46 @@ class DistrictsListView(APIView):
 
 class CentersListView(APIView):
     def get(self, request):
-        centers = {
-            DISTRICT_KEY(district_id): sorted(
-                json.loads(redis.get(DISTRICT_KEY(district_id)) or "[]"),
-                key=lambda center: center["name"],
+        state_id = request.query_params["state_id"]
+        if not state_id:
+            return Response(
+                {"error": "`state_id` query param should be provided"},
+                status=status.HTTP_400_BAD_REQUEST,
             )
-            for district_id in DISTRICT_IDS
-        }
-        updated = {
-            DISTRICT_UPDATE_TIME_KEY(district_id): (
-                (redis.get(DISTRICT_UPDATE_TIME_KEY(district_id)) or b"").decode(
-                    "utf-8"
+
+        data = {}
+
+        try:
+            with open(os.path.join(directory, f"districts/{state_id}.json")) as f:
+                districts = json.load(f)
+                district_ids = [
+                    district["district_id"] for district in districts["districts"]
+                ]
+            centers = {
+                DISTRICT_KEY(district_id): sorted(
+                    json.loads(redis.get(DISTRICT_KEY(district_id)) or "[]"),
+                    key=lambda center: center["name"],
                 )
-                or None
+                for district_id in district_ids
+            }
+            updated = {
+                DISTRICT_UPDATE_TIME_KEY(district_id): (
+                    (redis.get(DISTRICT_UPDATE_TIME_KEY(district_id)) or b"").decode(
+                        "utf-8"
+                    )
+                    or None
+                )
+                for district_id in district_ids
+            }
+            data = {
+                "updated": updated,
+                "centers": centers,
+            }
+
+        except FileNotFoundError:
+            return Response(
+                {"error": "Invalid `state_id`"},
+                status=status.HTTP_400_BAD_REQUEST,
             )
-            for district_id in DISTRICT_IDS
-        }
-        data = {
-            "districts": DISTRICT_IDS_AND_DISTRICTS,
-            "updated": updated,
-            "centers": centers,
-        }
+
         return Response(data)
